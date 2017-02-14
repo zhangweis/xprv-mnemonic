efe91476de210c5d78e4a7adb61bac6b44c0e30e
john <cdzhangw@gmail.com>
2017-02-14 15:27:55 +0800

    remove uglyfy

--- webpack.config.js
+++ webpack.config.js
@@ -57,12 +57,12 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   sourceMap: true,
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
